f5494a909a4b6ba1ec23c7302a1b90b9d8f33e87
Lourdes Benites <lourdesben95@gmail.com>
2022-06-08 19:10:37 -0600

    Create smaller sample data for traveler called travelerSampleData

---
+++ src/data/traveler-sample-data.js
@@ -0,0 +1,27 @@
+export const travelerSampleData = [
+    {
+        id: 7,
+        name: "Emmet Sandham",
+        travelerType: "relaxer"
+    },
+    {
+        id: 8,
+        name: "Carlin O'Reilly",
+        travelerType: "history buff"
+    },
+    {
+        id: 9,
+        name: "Natalee Deegin",
+        travelerType: "relaxer"
+    },
+    {
+        id: 10,
+        name: "Rickie Jodlowski",
+        travelerType: "relaxer"
+    },
+    {
+        id: 11,
+        name: "Joy Dovington",
+        travelerType: "history buff"
+    }
+];
