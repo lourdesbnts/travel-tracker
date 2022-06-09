1e23932313935870f2e31280b5c7ba2690b13350
Lourdes Benites <lourdesben95@gmail.com>
2022-06-09 13:06:24 -0600

    Add data to work with for destinationSampleData

---
+++ src/data/destination-sample-data.js
@@ -0,0 +1,41 @@
+export const destinationSampleData = [
+    {
+        id: 7,
+        destination: "Paris, France",
+        estimatedLodgingCostPerDay: 100,
+        estimatedFlightCostPerPerson: 395,
+        image: "https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+        alt: "city during the day time with eiffel tower"
+    },
+    {
+        id: 8,
+        destination: "Tokyo, Japan",
+        estimatedLodgingCostPerDay: 125,
+        estimatedFlightCostPerPerson: 1000,
+        image: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1971&q=80",
+        alt: "city with people walking in crosswalk and brightly lit shops at night"
+    },
+    {
+        id: 9,
+        destination: "Amsterdam, Netherlands",
+        estimatedLodgingCostPerDay: 100,
+        estimatedFlightCostPerPerson: 950,
+        image: "https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        alt: "canal with boats and trees and buildings along the side"
+    },
+    {
+        id: 10,
+        destination: "Toronto, Canada",
+        estimatedLodgingCostPerDay: 90,
+        estimatedFlightCostPerPerson: 450,
+        image: "https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80"
+    },
+    {
+        id: 11,
+        destination: "Mikonos, Greece",
+        estimatedLodgingCostPerDay: 140,
+        estimatedFlightCostPerPerson: 1000,
+        image: "https://images.unsplash.com/photo-1573783309724-e44b859f5a85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80",
+        alt: "cityscape along the water during the day"
+    }
+];
