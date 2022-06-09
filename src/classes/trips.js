0f91fc16f12221808df0ae6c75039e0204dedc99
Lourdes Benites <lourdesben95@gmail.com>
2022-06-09 10:31:17 -0600

    Build class for Trips.js

---
+++ src/classes/trips.js
@@ -0,0 +1,12 @@
+export class Trips {
+    constructor(trip) {
+        this.id = trip.id;
+        this.userID = trip.userID;
+        this.destinationID = trip.destinationID;
+        this.travelers = trip.travelers;
+        this.date = trip.date;
+        this.duration = trip.duration;
+        this.status = trip.status;
+        this.suggestedActivities = [];
+    }
+}
